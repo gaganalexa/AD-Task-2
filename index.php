2ed170d0d1f7cd654695a0f380f0a8fa643be00d
gaganalexa <202310537@fit.edu.ph>
2025-05-18 17:55:44 +0800

    feat(lamding): create index.php with initial HTML structure and php initializations

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include 'inc/data.php'; ?>
+<?php include 'inc/functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>WanderNow | Home</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h1>Welcome to WanderNow</h1>
+  <p>Your simple and elegant guide to exploring the world.</p>
+
+  <ul>
+    <li><a href="destinations.php">Destinations</a></li>
+    <li><a href="guides.php">Local Guides</a></li>
+    <li><a href="tips.php">Travel Tips</a></li>
+  </ul>
+</body>
+</html>
