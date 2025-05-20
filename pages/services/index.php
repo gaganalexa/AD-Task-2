3bd21234faa7751437034ddfb14720a40f454a90
gaganalexa <202310537@fit.edu.ph>
2025-05-20 09:18:30 +0800

    feat(services): add header and navigation links to services page

--- pages/services/index.php
+++ pages/services/index.php
@@ -6,6 +6,18 @@
     <title>Services</title>
 </head>
 <body>
-    
+    <header>
+        <h1>AMG Tours</h1>
+        <nav>
+            <ul>
+                <li><a href="../../index.php">Home</a></li>
+                <li><a href="../destinations/index.php">Destination</a></li>
+                <li><a href="../guides/index.php">Guides</a></li>
+                <li><a href="index.php">Services</a></li>
+                <li><a href="../tips/index.php">Tips</a></li>
+            </ul>
+        </nav>
+    </header>
+
 </body>
 </html>
