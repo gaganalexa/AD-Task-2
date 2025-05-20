7050d507b9c0287dfe67bf48cae91a6169ee6692
gaganalexa <202310537@fit.edu.ph>
2025-05-20 09:07:25 +0800

    feat(destinations): update title and add header with navigation links

--- pages/destinations/index.php
+++ pages/destinations/index.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Destinations</title>
 </head>
 <body>
-    
+    <header>
+        <h1>AMG Tours</h1>
+        <nav>
+            <ul>
+                <li><a href="../../index.php">Home</a></li>
+                <li><a href="index.php">Destination</a></li>
+                <li><a href="../guides/index.php">Guides</a></li>
+                <li><a href="../services/index.php">Services</a></li>
+                <li><a href="../tips/index.php">Tips</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
