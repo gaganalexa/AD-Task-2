4902716f29001a99e85c75a6b798e456fee6ed78
gaganalexa <202310537@fit.edu.ph>
2025-05-20 09:00:24 +0800

    feat(inc): add initial data structure for destinations, services, tips, and guides using data.php

---
+++ inc/data.php
@@ -0,0 +1,43 @@
+<?php
+    // Arrays
+    $destinations = [
+        [
+            'name' => 'Kyoto, Japan',
+            'description' => 'Immerse yourself in the timeless beauty and rich cultural heritage of Kyoto, Japan.',
+            'image' => '../../pages/destinations/assets/img/kyoto.png'
+        ],
+        [
+            'name' => 'Paris, France',
+            'description' => 'Experience the romance and grandeur of Paris, the "City of Lights."',
+            'image' => '../../pages/destinations/assets/img/paris.png'
+        ],
+        [
+            'name' => 'Cape Town, Africa',
+            'description' => 'Discover the breathtaking beauty and vibrant energy of Cape Town',
+            'image' => '../../pages/destinations/assets/img/cape-town.png'
+        ],
+    ];
+
+    $services = [
+        ['name' => 'Flight Booking', 'description' => 'Find and book your flights with ease.', 'icon' => 'fa-plane'],
+        ['name' => 'Hotel Reservation', 'description' => 'Secure your ideal accommodation for your trip.', 'icon' => 'fa-hotel'],
+        ['name' => 'Guided Tours', 'description' => 'Explore your destination with expert local guides.', 'icon' => 'fa-map-signs'],
+    ];
+
+    $tips = [
+        'Pack light and smart.',
+        'Always keep a digital copy of your documents.',
+        'Learn basic phrases of the local language.',
+        'Explore local cuisine.',
+        'Be mindful of local customs.',
+        'Stay hydrated.',
+    ];
+
+    // Dictionary (Associative Array)
+    $guides = [
+        'Chiaki Sato' => ['location' => 'Kyoto', 'image' => '../../pages/guides/assets/img/chiaki.png'],
+        'Benjamin Shah' => ['location' => 'Paris', 'image' => '../../pages/guides/assets/img/daniel.png'],
+        'Daniel Gallego' => ['location' => 'Cape Town', 'image' => '../../pages/guides/assets/img/benjamin.png'],
+    ];
+
+?>
