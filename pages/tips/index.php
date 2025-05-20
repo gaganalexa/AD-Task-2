1dcde89c7b50c17b650b13df7c6cf5d93ddd33a3
gaganalexa <202310537@fit.edu.ph>
2025-05-20 09:19:38 +0800

    feat(tips): add header and navigation links to tips page

--- pages/tips/index.php
+++ pages/tips/index.php
@@ -6,6 +6,17 @@
     <title>Tips</title>
 </head>
 <body>
-    
+    <header>
+        <h1>AMG Tours</h1>
+        <nav>
+            <ul>
+                <li><a href="../../index.php">Home</a></li>
+                <li><a href="../destinations/index.php">Destination</a></li>
+                <li><a href="../guides/index.php">Guides</a></li>
+                <li><a href="../services/index.php">Services</a></li>
+                <li><a href="index.php">Tips</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
