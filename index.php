47e8a5421cb3fe125a5297e09d1f19b40f9a7a70
gaganalexa <202310537@fit.edu.ph>
2025-05-20 08:57:55 +0800

    feat(landing): add header and navigation structure

--- index.php
+++ index.php
@@ -8,13 +8,16 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <h1>Welcome to WanderNow</h1>
-  <p>Your simple and elegant guide to exploring the world.</p>
-
-  <ul>
-    <li><a href="destinations.php">Destinations</a></li>
-    <li><a href="guides.php">Local Guides</a></li>
-    <li><a href="tips.php">Travel Tips</a></li>
-  </ul>
+    <header>
+        <h1>AMG Tours</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="pages/destinations/index.php">Destination</a></li>
+                <li><a href="pages/guides/index.php">Guides</a></li>
+                <li><a href="pages/services/index.php">Services</a></li>
+                <li><a href="pages/tips/index.php">Tips</a></li>
+            </ul>
+        </nav>
 </body>
 </html>
