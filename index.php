d3d6bf13a4bd1f94177a912518591b990f24b341
gaganalexa <202310537@fit.edu.ph>
2025-05-20 09:47:41 +0800

    refactor(landing): update stylesheet paths and remove unused includes

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<?php include 'inc/data.php'; ?>
-<?php include 'inc/functions.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <Home>Home</title>
-  <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <header>
